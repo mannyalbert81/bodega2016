d1d943fad16431d757308823c1a54f7fa9d59e8e
ManuelAlberto <manuel@masoft.net>
2016-07-05 19:01:34 -0500

    ddddd

---
+++ FrameworkMVC/view/ireports/ContGenerarSolicitudesReport.php
@@ -0,0 +1,30 @@
+<?php 
+#<?php 
+#Importas la librer�a PhpJasperLibrary
+include_once('PhpJasperLibrary/class/tcpdf/tcpdf.php');
+include_once("PhpJasperLibrary/class/PHPJasperXML.inc.php");
+include_once ('conexion.php');  
+#Conectas a la base de datos 
+$server  = server;
+$user    = user;
+$pass    = pass;
+$db      = db;
+$driver  = driver;
+ini_set('display_errors', 0);
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+$PHPJasperXML->debugsql=false;
+
+
+$id=$_GET['id_usuarios'];
+$PHPJasperXML = new PHPJasperXML("en","TCPDF");
+$PHPJasperXML->debugsql=false;
+$PHPJasperXML->arrayParameter=array("_id_usuarios"=>$id);
+$PHPJasperXML->load_xml_file("GenerarSolicitudesReport.jrxml");
+
+
+$PHPJasperXML->transferDBtoArray($server,$user,$pass,$db, $driver);
+$PHPJasperXML->outpage("I");
+
+?>
+
+
