cb0d48ff465e94530ee2241066401f5712fed8e1
mAYCOL <maycol@masoft.net>
2016-07-06 15:45:21 -0500

    qwq	w

--- FrameworkMVC/view/modulos/footer.php
+++ FrameworkMVC/view/modulos/footer.php
@@ -1,7 +1,7 @@
  <div class="navbar navbar-fixed-bottom"  >
        <footer  style="background:#2E2E2E; text-align: center; color: #FFFFFF;">
             <hr/>
-          Coactivas  - AllCoercive 2016 - <a href="http://www.masoft.net">www.masoft.net</a> - Copyright &copy; <?php echo  date("Y"); ?>
+          Bodegas  - AllDocstore 2016 - <a href="http://www.masoft.net">www.masoft.net</a> - Copyright &copy; <?php echo  date("Y"); ?>
         </footer>
   </div>    
   
