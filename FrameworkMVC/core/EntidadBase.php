f92d1ba1b05e8de8ebcfe15f625fdd694b9a06d3
Danny <danny@masoft.net>
2016-07-07 11:37:49 -0500

    notificacion

--- FrameworkMVC/core/EntidadBase.php
+++ FrameworkMVC/core/EntidadBase.php
@@ -549,7 +549,7 @@ class EntidadBase{
 				  public.usuarios, 
 				  public.tipo_notificacion";
     	
-    	$where="notificaciones.usuario_destino_notificaciones = usuarios.id_usuarios AND
+    	$where="notificaciones.usuario_origen_notificaciones = usuarios.id_usuarios AND
   				tipo_notificacion.id_tipo_notificacion = notificaciones.id_tipo_notificacion 
     			AND  notificaciones.visto_notificaciones='FALSE' 
     			AND notificaciones.usuario_destino_notificaciones='$id_usuario'";
